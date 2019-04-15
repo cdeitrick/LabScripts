87dfe0b8a10684edb3d1c2bd939fb20fcc843a09
cld100 <cld100@pitt.edu>
2019-04-15 14:32:49 -0400

    Updated the samplesheet generator.

--- sequencing_scraper.py
+++ sequencing_scraper.py
@@ -1,7 +1,29 @@
-import pandas
-from pathlib import Path
-from typing import Iterable, Callable, List, Dict
+import csv
 import datetime
+from pathlib import Path
+from typing import Callable, Dict, Iterable, List
+
+import pandas
+from loguru import logger
+
+COLUMNMAP = {
+	'I5_Index_ID': 'i5IndexId',
+	'I7_Index_ID': 'i7IndexId',
+	'Index_ID':    'indexId',
+	'Name':        'sampleName',
+	'NucleicAcid': 'nucleicAcid',
+	'Pop':         'pop',
+	'Project':     'projectName',
+	'SampleID':    'sampleId',
+	'Sample_ID':   'sampleId',
+	'Sample_Name': 'sampleName',
+	'Sample_Well': 'sampleWell',
+	'Species':     'species',
+	'Well':        'sampleWell',
+	'index':       'index',
+	'index2':      'index2'
+}
+
 
 def get_sample_id(path: Path):
 	sample_id = path.parent.name
@@ -26,7 +48,8 @@ def groupby(iterable: Iterable, by: Callable):
 			groups[key].append(i)
 	return groups
 
-def extract_date(path:Path)->datetime.date:
+
+def extract_date(path: Path) -> datetime.date:
 	for part in path.parts[::-1]:
 		match = part.isdigit()
 		if match:
@@ -36,10 +59,10 @@ def extract_date(path:Path)->datetime.date:
 			result = datetime.date(year = int(year), month = int(month), day = int(day))
 			return result
 
-def extract_date_from_sample_id(sample_id:str)->datetime.date:
+
+def extract_date_from_sample_id(sample_id: str) -> datetime.date:
 	if not isinstance(sample_id, str): return None
 	string = sample_id.split('_')[0]
-
 
 	try:
 		month = int(string[:2])
@@ -49,7 +72,9 @@ def extract_date_from_sample_id(sample_id:str)->datetime.date:
 	except ValueError:
 		result = None
 	return result
-def search_for_sample_sheets(folder:Path, index = 0)->List[Path]:
+
+
+def search_for_sample_sheets(folder: Path, index = 0) -> List[Path]:
 	sample_sheets = list()
 	if index > 3: return sample_sheets
 	for path in folder.iterdir():
@@ -58,6 +83,57 @@ def search_for_sample_sheets(folder:Path, index = 0)->List[Path]:
 		elif path.name == 'SampleSheet.csv':
 			sample_sheets.append(path)
 	return sample_sheets
+
+
+def find_all_sample_sheets(*paths) -> List[Path]:
+	""" Finds all sample sheets within the given folders. """
+	sample_sheets = list()
+	for path in paths:
+		files = list(path.glob('**/SampleSheet.csv'))
+		logger.info(f"Found {len(files)} samplesheets in folder {path}")
+		sample_sheets += files
+	return sample_sheets
+
+
+def combine_sample_sheets(filenames: List[Path]) -> pandas.DataFrame:
+	""" Combines the individual samplesheets into a single DataFrame."""
+	fieldnames = ['Sample ID', 'Sample_Name', 'Species', 'Project', 'NucleicAcid', 'Sample_Well', 'I7_Index_ID', 'index', 'I5_Index_ID', 'index2']
+	sample_sheet = list()
+	for filename in filenames:
+		reader = csv.DictReader(filename, fieldnames = fieldnames)
+		for line in reader:
+			# Find the header line, then break. The rest of the `reader` object should be the samples.
+			if line['Sample_ID'] == 'Sample_ID':
+				break
+		# Consume the rest of the `reader` object.
+		sample_sheet += list(reader)
+	df = pandas.DataFrame(sample_sheet)
+	df.columns = [COLUMNMAP[i] for i in df.columns]
+	return df
+
+
+def generate_combined_sample_sheet(*paths) -> pandas.DataFrame:
+	""" Generates a single samplesheet from all of the individual sample sheets."""
+
+	logger.info("Searching for all sample sheets...")
+	sample_sheets = find_all_sample_sheets(*paths)
+
+	logger.info("Combining all sample sheets...")
+	sample_sheet = combine_sample_sheets(sample_sheets)
+
+	billing_table = sample_sheet
+
+	billing_table['date'] = billing_table['sampleId'].apply(extract_date_from_sample_id)
+	logger.info(f"Found {len(billing_table)} samples")
+
+	billing_table = billing_table.drop_duplicates()
+	logger.info(f"Found {len(billing_table)} samples after removing duplicate sampleIds.")
+	current_date = datetime.datetime.now().date().isoformat()
+	output = Path(__file__).with_name(f"combined_sample_sheet.{current_date}.tsv")
+	billing_table.to_csv(str(output))
+
+	return billing_table
+
 
 class SequenceScraper:
 	def __init__(self, config_path: Path, dmux_path: Path):
@@ -103,7 +179,6 @@ class SequenceScraper:
 				sample_files = sample_map.get(sample_id, [])
 
 				for f in sample_files:
-
 					sample_file_row = {
 						'sampleDate':  extract_date(f),
 						'sampleId':    sample_id,
@@ -139,7 +214,6 @@ class SequenceScraper:
 		self.file_df = pandas.DataFrame(sample_file_list)
 
 	def to_excel(self, filename: Path):
-
 		filename = filename.with_suffix('.xlsx')
 		filename = str(filename)
 		writer = pandas.ExcelWriter(filename)
@@ -171,16 +245,16 @@ class SequenceScraper:
 			'index':       'index',
 			'index2':      'index2'
 		}
-		print("Searching for all sample sheets...")
-		#config_sample_sheets = list(self.config_path.glob(pattern))
+		logger.info("Searching for all sample sheets...")
+		# config_sample_sheets = list(self.config_path.glob(pattern))
 		config_sample_sheets = search_for_sample_sheets(self.config_path)
-		print("Found {} sample sheets in config_folder.".format(len(config_sample_sheets)))
-		#dmux_sample_sheets = list(self.dmux_path.glob(pattern))
+		logger.info(f"Found {len(config_sample_sheets)} sample sheets in config_folder.")
+		# dmux_sample_sheets = list(self.dmux_path.glob(pattern))
 		dmux_sample_sheets = search_for_sample_sheets(self.dmux_path)
-		print("Found {} sample sheets in dmux folder.".format(len(dmux_sample_sheets)))
+		logger.info(f"Found {len(dmux_sample_sheets)} sample sheets in dmux folder.")
 		all_sample_sheets = config_sample_sheets + dmux_sample_sheets
-		print("Found {} total.".format(len(all_sample_sheets)))
-		print("Combining all sample sheets...")
+		logger.info(f"Found {len(all_sample_sheets)} total.")
+		logger.info("Combining all sample sheets...")
 		billing_table = list()
 		for i in all_sample_sheets:
 			try:
@@ -196,10 +270,10 @@ class SequenceScraper:
 
 		billing_table = pandas.concat(billing_table)
 		billing_table['date'] = billing_table['sampleId'].apply(extract_date_from_sample_id)
-		print("Found {} samples".format(len(billing_table)))
-		#billing_table = billing_table.drop_duplicates(subset = 'sampleId', keep = 'first')
+		logger.info(f"Found {len(billing_table)} samples")
+		# billing_table = billing_table.drop_duplicates(subset = 'sampleId', keep = 'first')
 		billing_table = billing_table.drop_duplicates()
-		print("Found {} samples after removing duplicate sampleIds.".format(len(billing_table)))
+		logger.info(f"Found {len(billing_table)} samples after removing duplicate sampleIds.")
 
 		output = Path(__file__).with_name(f"combined_sample_sheet.{self.current_date}.tsv")
 		billing_table.to_csv(str(output), sep = "\t")
@@ -210,4 +284,5 @@ class SequenceScraper:
 if __name__ == "__main__":
 	config_path = Path("/home/data/raw")
 	dmux_folder = Path("/home/data/dmux")
-	SequenceScraper(config_path, dmux_folder)
+	table = generate_combined_sample_sheet(dmux_folder, config_path)
+
