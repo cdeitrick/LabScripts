5e2b36ce5f568fe9d9d37e0b4e3f3855c823f36a
cld100 <cld100@pitt.edu>
2019-07-01 10:32:27 -0400

    Added a script to merge two tables

---
+++ merge_tables.py
@@ -0,0 +1,76 @@
+import pandas
+from pathlib import Path
+import argparse
+def open_table(filename:Path, sheetname: str)->pandas.DataFrame:
+	if filename.suffix in ['.xlsx', 'xls']:
+		table = pandas.read_excel(filename, sheet_name = sheetname)
+	else:
+		table = pandas.read_csv(filename)
+	return table
+
+def merge_tables(left_filename:Path, right_filename:Path, left_sheet:str, right_sheet:str, on:str):
+	left_table = open_table(left_filename, left_sheet)
+	right_table = open_table(right_filename, right_sheet)
+
+	merged_table = left_table.merge(right_table, on = on)
+
+	return merged_table
+
+def create_parser(parameters = None):
+	parser = argparse.ArgumentParser()
+	parser.add_argument(
+		"-l", "--left-filename",
+		help = "The first spreadsheet to merge.",
+		action = 'store',
+		type = Path
+	)
+	parser.add_argument(
+		"-r", "--right-filename",
+		help = "The second spreadsheet to merge. Defaults to using the same spreadsheet file.",
+		action = 'store',
+		type = Path,
+		default = None
+	)
+	parser.add_argument(
+		'-1', '--left-sheetname',
+		help = 'The name of the sheet in the first spreadsheet to use. Uses the first sheet if no sheet name is given.',
+		default = 0,
+		dest = "left_sheet"
+	)
+	parser.add_argument(
+		"-2", "--right-sheetname",
+		help = 'The name of the sheet in the second spreadsheet to use. Uses the first sheet if no sheet name is given.',
+		default = 0,
+		dest = 'right_sheet'
+	)
+	parser.add_argument(
+		"-c", "--column",
+		help = "The column to merge both tables on.",
+		dest = 'column',
+	)
+	parser.add_argument(
+		"-o", "--output",
+		help = "The filename of the merged table.",
+		dest = "output",
+		type = Path
+	)
+	if parameters:
+		parsed_args = parser.parse_args(parameters)
+	else:
+		parsed_args = parser.parse_args()
+
+	if parsed_args.right_filename is None:
+		parsed_args.right_filename = parsed_args.left_filename
+
+	return parsed_args
+
+if __name__ == "__main__":
+	debug_args = ["-l", "/home/cld100/Downloads/RNA seq wrt WT(p_0.05).xlsx", "--right-sheetname", "A106P", "--column", "Gene_symbol"]
+	args = create_parser(debug_args)
+
+	# file:///home/cld100/Downloads/RNA seq wrt WT(p_0.05).xlsx
+	print(args)
+	merged_table = merge_tables(args.left_filename, args.right_filename, args.left_sheet, args.right_sheet, args.column)
+
+	print(merged_table.to_string())
+
